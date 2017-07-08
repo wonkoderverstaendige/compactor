f5d2df7c1c0a7083940bb1136408fb535dd7d856
Ronny Eichler <R.Eichler@science.ru.nl>
2017-07-08 16:57:51 +0200

    Handle conversion failures. Add progress bar. Exit with return value.

--- convert_async.py
+++ convert_async.py
@@ -5,30 +5,11 @@ import asyncio
 from asyncio.subprocess import PIPE, STDOUT
 import sys
 import time
+from tqdm import tqdm
 
 COMMAND = 'ffmpeg -y -hide_banner -i "{in_filepath}" -vf hqdn3d -c:v libx264 -crf 22 -preset veryfast "{out_filepath}"'
 OUTFILE = '{in_filepath.stem}_convert_x264_crf22_veryfast_hqdn3d.avi'
 DEFAULT_MAX_PROCS = 3
-
-
-def fmt_time(s, minimal=True):
-    """
-    Args:
-        s: time in seconds (float for fractional)
-        minimal: Flag, if true, only return strings for times > 0, leave rest outs
-    Returns: String formatted 99h 59min 59.9s, where elements < 1 are left out optionally.
-    """
-    ms = s - int(s)
-    s = int(s)
-    if s < 60 and minimal:
-        return "{s:02.3f}s".format(s=s + ms)
-
-    m, s = divmod(s, 60)
-    if m < 60 and minimal:
-        return "{m:02d}min {s:02.3f}s".format(m=m, s=s + ms)
-
-    h, m = divmod(m, 60)
-    return "{h:02d}h {m:02d}min {s:02.3f}s".format(h=h, m=m, s=s + ms)
 
 
 async def convert(in_filepath, output_dir):
@@ -38,7 +19,7 @@ async def convert(in_filepath, output_dir):
 
     size_in = in_filepath.stat().st_size
 
-    print('Starting {}, {:.2f} MB'.format(in_filepath, size_in / 1e6))
+    # tqdm.write('Starting {}, {:.2f} MB'.format(in_filepath, size_in / 1e6))
 
     p = await asyncio.create_subprocess_shell(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     stdout = (await p.communicate())[0].decode('utf-8')
@@ -47,14 +28,25 @@ async def convert(in_filepath, output_dir):
     with open(str(out_filepath) + '.ffmpeg.log', 'w') as ffmpeg_log:
         ffmpeg_log.write(stdout)
 
-    size_out = out_filepath.stat().st_size
     duration = time.time() - t_start
-    print("{}: {}, {:.2f} MB, compression: {:.2f}x".format(out_filepath, fmt_time(duration), size_out / 1e6,
-                                                           size_in / size_out))
-    return out_filepath
+
+    try:
+        size_out = out_filepath.stat().st_size
+        # tqdm.write("{}: {}, {:.2f} MB, compression: {:.2f}x".format(out_filepath, fmt_time(duration),
+        #                                                        size_out / 1e6,
+        #                                                        size_in / size_out))
+    except FileNotFoundError:
+        # tqdm.write("{}: Conversion failed!".format(in_filepath))
+        size_out = None
+
+    return size_out, in_filepath, out_filepath
 
 
 def main(target_path, preset, crf, max_procs=DEFAULT_MAX_PROCS):
+    targets = list(target_path.glob('*.avi'))
+    if not len(targets):
+        return None
+
     if sys.platform.startswith('win'):
         loop = asyncio.ProactorEventLoop()  # for subprocess' pipes on Windows
         asyncio.set_event_loop(loop)
@@ -66,26 +58,38 @@ def main(target_path, preset, crf, max_procs=DEFAULT_MAX_PROCS):
         Path.mkdir(output_dir)
         print("Made output directory", output_dir)
 
-    targets = list(target_path.glob('*.avi'))
-
     pending = []
-    while True:
-        pending = list(pending)
+    results = []
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    with tqdm(targets) as pbar:
+        while True:
+            pending = list(pending)
 
-        # Append tasks
-        while len(pending) < max_procs and len(targets):
-            pending.append(convert(targets.pop(), output_dir))
+            # Append tasks
+            while len(pending) < max_procs and len(targets):
+                pending.append(convert(targets.pop(), output_dir))
 
-        # Check for completed tasks
-        done, pending = loop.run_until_complete(asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED))
-        # for d in done:
-        #     print('Finished', d.result())
+            # Check for completed tasks
+            done, pending = loop.run_until_complete(asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED))
+            results.extend([d.result() for d in  done])
+            pbar.update(len(done))
 
-        # Stop when all done
-        if not len(pending) and not len(targets):
-            break
+            # Stop when all done
+            if not len(pending) and not len(targets):
+                break
 
     loop.close()
+
+    # Handle failed conversions
+    failures = [result for result in results if not result[0]]
+    if failures:
+        error_log_path = output_dir / 'errors_{}.log'.format(timestr)
+        print('Failed to convert {} file(s)! See {} for details.'.format(len(failures), error_log_path))
+        with open(error_log_path, 'w+') as error_log:
+            for failure in failures:
+                error_log.write(str(failure[1]))
+    sys.exit(len(failures))
+
 
 
 if __name__ == "__main__":
